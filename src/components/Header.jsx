90e4d85c441ae07ba27c2a907c706960ea6a676a
Luciano Sonoda <brluson@gmail.com>
2026-02-28 11:52:10 -0300

    Fix logo loading and update form action

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import '../styles/Header.css';
+import logoChico from '../assets/logo-chico.png';
 
 export function Header() {
     return (
         <header className="header glass-panel">
             <div className="header-content container">
                 <a href="#home" className="logo">
-                    <img src="/logo-chico.png" alt="SAGASON" className="header-logo-img" />
+                    <img src={logoChico} alt="SAGASON" className="header-logo-img" />
                 </a>
                 <nav className="nav">
                     <a href="#home">Inicio</a>
